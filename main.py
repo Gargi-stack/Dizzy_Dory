2b8736b25fde35d72a0beba85a33ff3bc8589afd
Gargi-stack <gargisardessai1@gmail.com>
2025-04-29 15:46:16 +0530

    Update main.py
    i hv added obstacles need to replace them with your corals and fishes.

--- main.py
+++ main.py
@@ -2,6 +2,14 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+import random  # NEW
+
+# obstacle variables
+obstacle_width = 70
+obstacle_gap = 180  # vertical space between top and bottom seaweed
+obstacle_frequency = 1500  # milliseconds
+last_obstacle_time = pygame.time.get_ticks()
+obstacles = []
 
 clock = pygame.time.Clock()
 fps = 60
@@ -17,7 +25,7 @@ bg_scroll = 0
 scroll_speed = 4
 
 # load and scale background
-bg = pygame.image.load('img/bg.png').convert()
+bg = pygame.image.load('bg.png').convert()
 bg = pygame.transform.scale(bg, (screen_width, screen_height))
 
 
@@ -29,7 +37,7 @@ class Fish(pygame.sprite.Sprite):
         self.index = 0
         self.counter = 0
         for num in range(1, 4):
-            img = pygame.image.load(f'img/fish{num}.png').convert_alpha()
+            img = pygame.image.load(f'fish{num}.png').convert_alpha()
             img = pygame.transform.scale(img, (60, 45))
             self.images.append(img)
         self.image = self.images[self.index]
@@ -61,6 +69,13 @@ class Fish(pygame.sprite.Sprite):
             self.rect.top = 0
             self.velocity = 0
 
+# function to create seaweed obstacles
+def create_obstacle():
+    height = random.randint(100, screen_height - obstacle_gap - 100)
+    top_rect = pygame.Rect(screen_width, 0, obstacle_width, height)
+    bottom_rect = pygame.Rect(screen_width, height + obstacle_gap, obstacle_width, screen_height - height - obstacle_gap)
+    return (top_rect, bottom_rect)
+
 
 # create fish
 fish_group = pygame.sprite.Group()
@@ -84,6 +99,23 @@ while run:
     # update and draw fish
     fish_group.update()
     fish_group.draw(screen)
+
+    # spawn new obstacle
+    current_time = pygame.time.get_ticks()
+    if current_time - last_obstacle_time > obstacle_frequency:
+      obstacles.append(create_obstacle())
+      last_obstacle_time = current_time
+
+    # move and draw obstacles
+    for top_rect, bottom_rect in obstacles:
+       top_rect.x -= scroll_speed
+       bottom_rect.x -= scroll_speed
+       pygame.draw.rect(screen, (0, 255, 0), top_rect)      # green top seaweed
+       pygame.draw.rect(screen, (0, 255, 0), bottom_rect)   # green bottom seaweed
+
+    # remove obstacles that go off screen
+    obstacles = [pair for pair in obstacles if pair[0].x > -obstacle_width]
+
 
     # event handling
     for event in pygame.event.get():
